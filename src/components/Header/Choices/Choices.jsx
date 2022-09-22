98455717cb0fe07eec830003f5886561cea926a7
smallwisp <4ubarov96@mail.ru>
2022-09-23 01:26:08 +0300

    changes in Choices

--- src/components/Header/Choices/Choices.jsx
+++ src/components/Header/Choices/Choices.jsx
@@ -1,7 +1,23 @@
+import React from 'react';
 import style from './Choices.module.css';
 
+const holiday = {
+  newyear: 'Новый год',
+  birthdayWomen: 'День рождения Ж',
+  birthdayMen: 'День рождения М',
+  womensday: '8 марта',
+  nknowledgeday: 'День знаний',
+};
+
 const Choices = () => (
-  <button className={style.button}>Выбрать праздник</button>
+  <div className={style.wrapper}>
+    <button className={style.button}>Выбрать праздник</button>
+    <ul className={style.list}>
+      {Object.entries(holiday).map(item => (
+        <li className={style.item} key={item[0]}>{item[1]}</li>
+      ))}
+    </ul>
+  </div>
 );
 
 export default Choices;
