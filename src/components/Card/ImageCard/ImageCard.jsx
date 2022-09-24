e280780ce57cf40457cca94d2a01079f5b677af2
smallwisp <4ubarov96@mail.ru>
2022-09-24 20:46:47 +0300

    second_day completed

--- src/components/Card/ImageCard/ImageCard.jsx
+++ src/components/Card/ImageCard/ImageCard.jsx
@@ -1,5 +1,11 @@
-const ImageCard = (props) => (
-  <img src={props.img} alt="Фон" width={840} height={520} />
-);
+import { useContext } from "react";
+import { imgContext } from "../../../context/imgContext";
+
+const ImageCard = (props) => {
+  const img = useContext(imgContext);
+  return (
+    <img src={props.img} alt="Фон" width={840} height={520} />
+  )
+};
 
 export default ImageCard;
--- src/components/Header/Choices/Choices.jsx
+++ src/components/Header/Choices/Choices.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Choices.module.css';
 
-const holiday = {
+const holidays = {
   newyear: 'Новый год',
   birthdayWomen: 'День рождения Ж',
   birthdayMen: 'День рождения М',
@@ -9,15 +9,40 @@ const holiday = {
   nknowledgeday: 'День знаний',
 };
 
-const Choices = () => (
-  <div className={style.wrapper}>
-    <button className={style.button}>Выбрать праздник</button>
-    <ul className={style.list}>
-      {Object.entries(holiday).map(item => (
-        <li className={style.item} key={item[0]}>{item[1]}</li>
-      ))}
-    </ul>
+const Choices = () => {
+  const [isOpenChoices, setIsOpenChoices] = useState(false);
+  const [holiday, setHoliday] = useState('Выбрать праздник');
+
+  const toggleChoices = () => {
+    setIsOpenChoices(!isOpenChoices)
+  };
+
+  const changeHoliday = (title) => {
+    setHoliday(title);
+    toggleChoices();
+  };
+
+  return (
+    <div className={style.wrapper}>
+      <button className={style.button} onClick={toggleChoices}>{holiday}</button>
+    {isOpenChoices && (
+        <ul className={style.list}>
+          {Object.entries(holidays).map(item => (
+            <li
+              className={style.item}
+              key={item[0]}
+              onClick={() => {
+                changeHoliday(item[1])
+              }}
+            >
+              {item[1]}
+            </li>
+          ))}
+        </ul>
+      )
+    }
   </div>
-);
+  )
+};
 
 export default Choices;
