6b66a8eb3fedb8e2f8fe0a3af26c6db6a19a4271
smallwisp <4ubarov96@mail.ru>
2022-09-30 10:37:58 +0300

    added giving data from server

--- src/components/Header/Choices/Choices.jsx
+++ src/components/Header/Choices/Choices.jsx
@@ -1,18 +1,24 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { URI_API } from '../../../const/const';
+import { holidaysContext } from '../../../context/holidaysContext';
 import style from './Choices.module.css';
-
-const holidays = {
-  newyear: 'Новый год',
-  birthdayWomen: 'День рождения Ж',
-  birthdayMen: 'День рождения М',
-  womensday: '8 марта',
-  nknowledgeday: 'День знаний',
-};
 
 const Choices = () => {
   const [isOpenChoices, setIsOpenChoices] = useState(false);
-  const [holiday, setHoliday] = useState('Выбрать праздник');
+  const {holiday, setHoliday} = useContext(holidaysContext);
+  const [holidays, setHolidays] = useState({});
 
+  useEffect(() => {
+    fetch(URI_API)
+      .then(response => {
+        if (response.status !== 200) {
+          throw new Error(response.status)
+        }
+        return response.json()
+      })
+      .then(data => setHolidays(data))
+      .catch(err => console.error(err));
+  }, []);
   const toggleChoices = () => {
     setIsOpenChoices(!isOpenChoices)
   };
---
+++ src/const/const.js
@@ -0,0 +1,2 @@
+export const URI_API = 'https://vanilla-legend-factory.glitch.me/api/';
+
