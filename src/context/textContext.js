12b1b2415f76cc820b825e932777bbfe29af6d3f
smallwisp <4ubarov96@mail.ru>
2022-10-04 00:56:30 +0300

    start remaking app with redux

--- src/context/textContext.js
+++ src/context/textContext.js
@@ -1,12 +1,12 @@
-import { createContext, useContext } from "react";
+import { createContext } from "react";
 import { useFetch } from "../hooks/useFetch";
 import { URI_API } from "../const/const";
-import { holidaysContext } from "./holidaysContext";
+import { useSelector } from "react-redux";
 
 export const textContext = createContext({});
 
 export const TextContextProvider = ({children}) => {
-  const {holiday} = useContext(holidaysContext);
+  const holiday = useSelector(state => state.holidays.holiday);
   const [{text}] = useFetch(holiday ? `${URI_API}text/${holiday}` : '');
 
   return (
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import holidaysReducer from "./holidaysSlice";
+
+export const store = configureStore({
+  reducer: {
+    holidays: holidaysReducer,
+  }
+});
